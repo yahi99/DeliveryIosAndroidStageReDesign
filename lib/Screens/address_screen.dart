cf6441e0b6ce5109ff9193fdb96114fbf652823b
android_client <seturidee@mail.ru>
2020-12-09 17:06:19 +0300

    lala

--- lib/Screens/address_screen.dart
+++ lib/Screens/address_screen.dart
@@ -633,10 +633,10 @@ class PageState extends State<PageScreen> {
                             }
                             currentUser.cartDataModel.cart.clear();
                             currentUser.cartDataModel.saveData();
-                            homeScreenKey = new GlobalKey<HomeScreenState>();
+//                            homeScreenKey = new GlobalKey<HomeScreenState>();
                             Navigator.of(context).pushAndRemoveUntil(
                                 MaterialPageRoute(
-                                    builder: (context) => HomeScreen()),
+                                    builder: (context) => OrderSuccessScreen(name: currentUser.name,)),
                                     (Route<dynamic> route) => false);
                           } else {
                             emptyFields(context);
@@ -1536,6 +1536,88 @@ class DestinationPointsSelectorState extends State<DestinationPointsSelector> {
             children: widgetsList,
           ),
         ),
+      ),
+    );
+  }
+}
+
+class OrderSuccessScreen extends StatefulWidget {
+  final String name;
+
+  OrderSuccessScreen({Key key, this.name}) : super(key: key);
+
+  @override
+  OrderSuccessScreenState createState() {
+    return new OrderSuccessScreenState(name);
+  }
+}
+
+class OrderSuccessScreenState extends State<OrderSuccessScreen> {
+  final String name;
+
+  OrderSuccessScreenState(this.name);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          color: Colors.white,
+          child: Stack(
+            children: <Widget>[
+              Align(
+                alignment: Alignment.center,
+                child: Padding(
+                  padding: EdgeInsets.only(bottom: 20),
+                  child: Text(name + ', ваш заказ принят! ',
+                    style: TextStyle(
+                      fontSize: 24
+                    ),
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: Padding(
+                  padding: EdgeInsets.only(top: 60),
+                  child: Text('Вы можете отследить его статус\nна главной странце!',
+                    style: TextStyle(
+                        fontSize: 18
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  decoration: BoxDecoration(color: Colors.white),
+                  child: Padding(
+                    padding:
+                    EdgeInsets.only(bottom: 15, left: 15, right: 15, top: 15),
+                    child: FlatButton(
+                      child: Text(
+                        'Продолжить',
+                        style: TextStyle(color: Colors.white, fontSize: 18),
+                      ),
+                      color: Color(0xFFFE534F),
+                      splashColor: Colors.redAccent,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      padding: EdgeInsets.only(
+                          left: 110, top: 20, right: 110, bottom: 20),
+                      onPressed: () {
+                        homeScreenKey = new GlobalKey<HomeScreenState>();
+                        Navigator.of(context).pushAndRemoveUntil(
+                            MaterialPageRoute(builder: (context) => HomeScreen()),
+                                (Route<dynamic> route) => false);
+                      },
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          )
       ),
     );
   }
